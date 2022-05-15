43aa373cd24ca2b6762bc3fc66466e0aca12933a
iLucaH <lucahautrive@gmail.com>
2022-05-15 13:16:29 +1000

    Created a new animation: CONE.

--- src/main/java/me/ilucah/virtualbackpacks/utils/particle/ParticleAnimator.java
+++ src/main/java/me/ilucah/virtualbackpacks/utils/particle/ParticleAnimator.java
@@ -14,71 +14,33 @@ public class ParticleAnimator {
 
     private static final Random random = new Random(System.nanoTime());
 
-    public static Random getRandom() {
-        return random;
-    }
+    private static BiConsumer<Player, ParticleEffect> reefer() {
+        return ((player, particle) -> new BukkitRunnable() {
+            double phi = 0;
 
-    public static void disco(final Player player, ParticleEffect particle) {
-        double radius = 3;
-        int particles = 30;
-        boolean sphere = false, reverse = false;
-        final Location location = player.getLocation();
-        new BukkitRunnable() {
-            float iterations = 0;
-
-            @Override
             public void run() {
-                iterations++;
-                for (int i = 0; i < particles; i++) {
-                    Vector vector = getRandomVector().multiply(radius);
-                    if (!sphere) {
-                        if (reverse) vector.setY(Math.abs(vector.getY()) * -1);
-                        else vector.setY(Math.abs(vector.getY()));
+                phi = phi + Math.PI / 8;
+                double x, y, z;
+                Location location1 = player.getLocation();
+                for (double t = 0; t <= Math.PI; t = t + Math.PI / 16) {
+                    for (double i = 0; i <= 1; i = i + 1) {
+                        x = 0.4 * (2 * Math.PI - t) * 0.5 * Math.cos(t + phi + i * Math.PI);
+                        y = 0.5 * t;
+                        z = 0.4 * (2 * Math.PI - t) * 0.5 * Math.sin(t + phi + i * Math.PI);
+                        location1.add(x, y, z);
+                        particle.display(location1, player);
+                        location1.subtract(x, y, z);
                     }
-                    location.add(vector);
-                    particle.display(location, player);
-                    location.subtract(vector);
                 }
-                if (iterations >= 20) {
-                    cancel();
+                if (phi > 2 * Math.PI) {
+                    this.cancel();
                 }
             }
-        }.runTaskTimer(VirtualBackpacks.getInstance(), 0, 1);
+        }.runTaskTimer(VirtualBackpacks.getInstance(), 0, 3));
     }
 
-    public static BiConsumer<Player, ParticleEffect> reefer() {
-        return ((player, particle) -> {
-            new BukkitRunnable() {
-                double phi = 0;
-
-                public void run() {
-                    phi = phi + Math.PI / 8;
-                    double x, y, z;
-
-                    Location location1 = player.getLocation();
-                    for (double t = 0; t <= Math.PI; t = t + Math.PI / 16) {
-                        for (double i = 0; i <= 1; i = i + 1) {
-                            x = 0.4 * (2 * Math.PI - t) * 0.5 * Math.cos(t + phi + i * Math.PI);
-                            y = 0.5 * t;
-                            z = 0.4 * (2 * Math.PI - t) * 0.5 * Math.sin(t + phi + i * Math.PI);
-                            location1.add(x, y, z);
-                            particle.display(location1, player);
-                            location1.subtract(x, y, z);
-                        }
-
-                    }
-
-                    if (phi > 2 * Math.PI) {
-                        this.cancel();
-                    }
-                }
-            }.runTaskTimer(VirtualBackpacks.getInstance(), 0, 3);
-        });
-    }
-
-    public static BiConsumer<Player, ParticleEffect> rings() {
+    private static BiConsumer<Player, ParticleEffect> rings() {
         return ((player, particle) -> new BukkitRunnable() {
-
             double alpha = 0;
             float t = 0;
             Location loc = player.getLocation();
@@ -90,14 +52,13 @@ public class ParticleAnimator {
                 Location secondLocation = loc.clone().add(Math.cos(alpha + Math.PI), Math.sin(alpha) + 1, Math.sin(alpha + Math.PI));
                 particle.display(firstLocation, player);
                 particle.display(secondLocation, player);
-
                 if (t >= 30)
                     cancel();
             }
         }.runTaskTimer(VirtualBackpacks.getInstance(), 0, 1));
     }
 
-    public static BiConsumer<Player, ParticleEffect> stomp() {
+    private static BiConsumer<Player, ParticleEffect> stomp() {
         return ((player, particle) -> new BukkitRunnable() {
             double t = Math.PI / 4;
             Location loc = player.getLocation();
@@ -111,9 +72,7 @@ public class ParticleAnimator {
                     loc.add(x, y, z);
                     particle.display(loc, player);
                     loc.subtract(x, y, z);
-
                     theta = theta + Math.PI / 64;
-
                     x = t * Math.cos(theta);
                     y = 2 * Math.exp(-0.1 * t) * Math.sin(t) + 1.5;
                     z = t * Math.sin(theta);
@@ -128,24 +87,19 @@ public class ParticleAnimator {
         }.runTaskTimer(VirtualBackpacks.getInstance(), 0, 1));
     }
 
-    public static BiConsumer<Player, ParticleEffect> star() {
+    private static BiConsumer<Player, ParticleEffect> star() {
         return ((player, particle) -> {
-
             int particles = 50;
             float spikeHeight = 3.5f;
             int spikesHalf = 3;
             float innerRadius = 0.5f;
-
             Location location = player.getLocation();
-
             new BukkitRunnable() {
-
                 int iterations = 0;
 
                 @Override
                 public void run() {
                     iterations++;
-
                     float radius = 3 * innerRadius / 1.73205f;
                     for (int i = 0; i < spikesHalf * 2; i++) {
                         double xRotation = i * Math.PI / spikesHalf;
@@ -166,7 +120,6 @@ public class ParticleAnimator {
                             location.subtract(v);
                         }
                     }
-
                     if (iterations >= 30) {
                         cancel();
                     }
@@ -175,7 +128,7 @@ public class ParticleAnimator {
         });
     }
 
-    public static BiConsumer<Player, ParticleEffect> frostLord() {
+    private static BiConsumer<Player, ParticleEffect> frostLord() {
         return ((player, particle) -> new BukkitRunnable() {
             double t = 0;
             double pi = Math.PI;
@@ -190,7 +143,6 @@ public class ParticleAnimator {
                     loc.add(x, y, z);
                     particle.display(loc, player);
                     loc.subtract(x, y, z);
-
                     if (t >= 4 * pi) {
                         this.cancel();
                         loc.add(x, y, z);
@@ -202,7 +154,7 @@ public class ParticleAnimator {
         }.runTaskTimer(VirtualBackpacks.getInstance(), 0, 1));
     }
 
-    public static BiConsumer<Player, ParticleEffect> disco() {
+    private static BiConsumer<Player, ParticleEffect> disco() {
         return ((player, particle) -> {
             double radius = 3;
             int particles = 30;
@@ -232,12 +184,44 @@ public class ParticleAnimator {
         });
     }
 
+    private static BiConsumer<Player, ParticleEffect> cone() {
+        return (player, particle) -> {
+            float lengthGrow = .05f;
+            double angularVelocity = Math.PI / 16;
+            int particles = 10;
+            float radiusGrow = 0.006f;
+            double rotation = 0;
+            new BukkitRunnable() {
+                int iteration = 0;
+                Location location = player.getLocation();
+                @Override
+                public void run() {
+                    for (int x = 0; x < particles; x++) {
+                        double angle = iteration * angularVelocity + rotation;
+                        float radius = iteration * radiusGrow;
+                        float length = iteration * lengthGrow;
+                        Vector v = new Vector(Math.cos(angle) * radius, length, Math.sin(angle) * radius);
+                        rotateAroundAxisX(v, (location.getPitch() + 90) * (180f / Math.PI));
+                        rotateAroundAxisY(v, -location.getYaw() * (180f / Math.PI));
+
+                        location.add(v);
+                        particle.display(location, player);
+                        location.subtract(v);
+                        iteration++;
+                    }
+                    if (iteration >= 200) {
+                        cancel();
+                    }
+                }
+            }.runTaskTimer(VirtualBackpacks.getInstance(), 0, 1);
+        };
+    }
+
     public static Vector getRandomVector() {
         double x, y, z;
         x = random.nextDouble() * 2 - 1;
         y = random.nextDouble() * 2 - 1;
         z = random.nextDouble() * 2 - 1;
-
         return new Vector(x, y, z).normalize();
     }
 
@@ -270,12 +254,13 @@ public class ParticleAnimator {
 
     public enum ParticleAnimation {
 
-        DISCO(ParticleAnimator.disco()),
-        REEFER(ParticleAnimator.reefer()),
-        RINGS(ParticleAnimator.rings()),
-        STOMP(ParticleAnimator.stomp()),
-        FROST_LORD(ParticleAnimator.frostLord()),
-        STAR(ParticleAnimator.star());
+        FROST_LORD(frostLord()),
+        REEFER(reefer()),
+        STOMP(stomp()),
+        DISCO(disco()),
+        RINGS(rings()),
+        STAR(star()),
+        CONE(cone());
 
         private final BiConsumer<Player, ParticleEffect> animation;
 
